593a99420bd1b012be585677952709530d4c9502
Arthurdlc25 <arthurdlc25@hotmail.com>
2025-09-23 23:13:15 -0500

    VERSION1

---
+++ src/main/java/com/upc/puntualito/PuntualitoApplication.java
@@ -0,0 +1,13 @@
+package com.upc.puntualito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuntualitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PuntualitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/upc/puntualito/PuntualitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.puntualito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuntualitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
